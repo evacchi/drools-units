dde33b0e98c8c633d63608f4a40bdb00756d4369
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-11-06 14:36:49 +0100

    wip

--- drools/src/test/java/org/drools/units/example/RuleUnitTest.java
+++ drools/src/test/java/org/drools/units/example/RuleUnitTest.java
@@ -10,7 +10,6 @@ import static org.kie.api.UnitInstance.State.Completed;
 import static org.kie.api.UnitInstance.State.Created;
 import static org.kie.api.UnitInstance.State.Entering;
 import static org.kie.api.UnitInstance.State.Exiting;
-import static org.kie.api.UnitInstance.State.Resuming;
 import static org.kie.api.UnitInstance.State.Running;
 
 public class RuleUnitTest {
